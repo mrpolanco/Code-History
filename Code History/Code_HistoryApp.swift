a1330ebf66fa72e8d540224fdfb4e59bbe6ce502
Rich Polanco <richardpolanco@gmail.com>
2023-02-05 17:39:13 -0600

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Rich Polanco on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
