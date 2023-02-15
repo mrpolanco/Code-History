a628c137d6bfc71316a64398bdad707007c9c9d4
Rich Polanco <richardpolanco@gmail.com>
2023-02-14 19:04:35 -0600

    Questions Model (MVVM)

---
+++ Code History/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Rich Polanco on 2/14/23.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswersIndex: Int
+}
