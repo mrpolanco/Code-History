6d5261afa2def321582f5e9b54f03fd7bf2f033b
Rich Polanco <richardpolanco@gmail.com>
2023-02-18 20:58:01 -0600

    API logic for Game model

--- Code History/Models/Question.swift
+++ Code History/Models/Question.swift
@@ -7,8 +7,27 @@
 
 import Foundation
 
-struct Question {
+struct Question: Hashable {
     let questionText: String
     let possibleAnswers: [String]
-    let correctAnswersIndex: Int
+    let correctAnswerIndex: Int
+
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                 possibleAnswers: [
+                    "Steve Jobs",
+                    "Linus Torvalds",
+                    "Bill Gates",
+                    "Tim Berners-Lee"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                 possibleAnswers: [
+                    "Simula",
+                    "Python",
+                    "Swift",
+                    "C"
+                 ],
+                 correctAnswerIndex: 0)
+    ]
 }
